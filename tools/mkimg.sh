acfe23fc34879f53ab8bc16a1b2e721e5b683c82
Christophe Lincoln <pankso@slitaz.org>
2014-03-22 20:14:49 +0100

    Add tools/mkimg.sh - (not yet finished)

---
+++ tools/mkimg.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Create slitaz-arm.img
+#
+. /lib/libtaz.sh
+
+[ "$size" ] || size=80
+
+distro="distro"
+. $distro/rootfs/etc/slitaz/flavor.conf || exit 1
+flavor="$FLAVOR"
+#img="slitaz-$flavor-$(date +%Y%m%d)"
+img=slitaz-arm.img
+
+cd ${distro}
+echo "Creating disk image..."
+dd if=/dev/zero of=${img} bs=1M count=${size}
+
+device=`kpartx -va $img | sed -E 's/.*(loop[0-9])p.*/\1/g' | head -1`
+device="/dev/mapper/${device}"
+bootp=${device}p1
+rootp=${device}p2
